75c45b11e9b2d7ed2f7d2085a8eb2f46899e473e
Hashirkhan24 <khn24202@gmail.com>
2024-05-11 20:23:23 +0530

    Add Polariod Frame to photography section

--- components/photography.tsx
+++ components/photography.tsx
@@ -6,6 +6,7 @@ import { motion, useAnimation } from "framer-motion"; // Importing motion and us
 import { naturePhotos } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "./section-heading";
+import { BsPinAngleFill } from "react-icons/bs";
 
 const Photography = () => {
   const [activeImage, setActiveImage] = useState(0);
@@ -35,33 +36,36 @@ const Photography = () => {
     <section ref={ref} id="hobbies" className="scroll-mt-28">
       <SectionHeading>My Hobby - Capturing the Blooms!</SectionHeading>
       <main className="grid place-items-center w-full mx-auto max-w-screen-xl mb-20 sm:mb-32">
-        <motion.div
-          className="relative sm:w-[75vb] sm:h-[75vh] w-[25vb] h-[25vh] overflow-hidden rounded-md shadow-lg"
-          initial="hidden"
-          animate="visible"
-          variants={containerVariants}
-        >
-          {naturePhotos.map((elem, idx) => (
-            <motion.div
-              key={idx}
-              className={`absolute top-0 left-0 w-full h-full`}
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: idx === activeImage ? 1 : 0,
-                zIndex: idx === activeImage ? 10 : 0,
-              }}
-              transition={{ duration: 0.5 }}
-            >
-              <Image
-                src={elem.link}
-                alt=""
-                layout="fill"
-                objectFit="cover"
-                className="rounded-md"
-              />
-            </motion.div>
-          ))}
-        </motion.div>
+        <div className=" mt-4 px-[1rem] pt-[1rem] pb-[5rem] bg-gradient-to-tr from-gray-100 via-gray-200 to-gray-100 dark:from-gray-800 dark:via-gray-900 dark:to-gray-800 rotate-6 flex flex-col items-center justify-center">
+          <BsPinAngleFill className="text-slate-800 dark:text-slate-400 text-3xl gap-2 mt-[-2rem] mb-[1rem]" />
+          <motion.div
+            className="relative sm:w-[55vb] sm:h-[55vh] w-[25vb] h-[25vh] overflow-hidden shadow-lg"
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+          >
+            {naturePhotos.map((elem, idx) => (
+              <motion.div
+                key={idx}
+                className={`absolute top-0 left-0 w-full h-full`}
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: idx === activeImage ? 1 : 0,
+                  zIndex: idx === activeImage ? 10 : 0,
+                }}
+                transition={{ duration: 0.5 }}
+              >
+                <Image
+                  src={elem.link}
+                  alt=""
+                  layout="fill"
+                  objectFit="cover"
+                  // className="rounded-md"
+                />
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
       </main>
     </section>
   );
